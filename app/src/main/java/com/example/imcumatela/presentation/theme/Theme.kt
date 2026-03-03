324200a7b071152a480b56de7ed3de9d4b4275cb
BeatrisO <beatris_oliveira97@hotmail.com>
2026-03-03 15:08:56 -0300

    Corrige import de Typography no Theme

--- app/src/main/java/com/example/imcumatela/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/imcumatela/presentation/theme/Theme.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Typography
 
 private val LightColorScheme = lightColorScheme(
     primary = Color(0xFF2D7A63),
