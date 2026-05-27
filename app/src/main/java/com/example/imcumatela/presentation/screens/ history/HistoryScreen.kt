cf8a7c27ce9e0cc8af22c2bc6e3bfb48b8fdb278
Beatris Oliveira <beatris_oliveira97@hotmail.com>
2026-05-27 13:42:21 -0300

    Renomeando pasta HistoryScreen.kt

--- app/src/main/java/com/example/imcumatela/presentation/screens/ history/HistoryScreen.kt
+++
@@ -1,4 +0,0 @@
-package com.example.imcumatela.presentation.screens.` history`
-
-class HistoryScreen {
-}
---
+++ app/src/main/java/com/example/imcumatela/presentation/screens/history/HistoryScreen.kt
@@ -0,0 +1,4 @@
+package com.example.imcumatela.presentation.screens.history
+
+class HistoryScreen {
+}
